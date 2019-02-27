ab10b84317c9a89dcec133aa9b91df626f3c6693
Pablo Adriaensens Albiñana <pablo.adriaensens@gmail.com>
2019-02-27 11:16:44 +0100

    Showing the info of the Noticias component

--- .next/static/development/pages/index.js
+++ .next/static/development/pages/index.js
@@ -197,21 +197,96 @@ __webpack_require__.r(__webpack_exports__);
 var _jsxFileName = "/Users/Pablo/Documents/Udemy/React/Proyectos/bitcoinapp/components/Noticia.js";
 
 
-var Noticia = function Noticia() {
+var Noticia = function Noticia(props) {
+  var _props$noticia = props.noticia,
+      urlToImage = _props$noticia.urlToImage,
+      url = _props$noticia.url,
+      title = _props$noticia.title,
+      description = _props$noticia.description,
+      source = _props$noticia.source;
+  var imagen;
+
+  if (urlToImage !== '') {
+    imagen = react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("img", {
+      src: urlToImage,
+      alt: title,
+      className: "card-img-top",
+      __source: {
+        fileName: _jsxFileName,
+        lineNumber: 8
+      },
+      __self: this
+    });
+  } else {
+    imagen = react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("p", {
+      className: "text-center my-5",
+      __source: {
+        fileName: _jsxFileName,
+        lineNumber: 10
+      },
+      __self: this
+    }, "Imagen No Disponible");
+  }
+
   return react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
     className: "col-md-6 col-12 mb-4",
     __source: {
       fileName: _jsxFileName,
-      lineNumber: 3
+      lineNumber: 14
     },
     __self: this
-  }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("p", {
+  }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
+    className: "card",
     __source: {
       fileName: _jsxFileName,
-      lineNumber: 4
+      lineNumber: 15
     },
     __self: this
-  }, "desde Noticia.js"));
+  }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
+    className: "card-image",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 16
+    },
+    __self: this
+  }, imagen), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
+    className: "card-body",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 19
+    },
+    __self: this
+  }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("h3", {
+    className: "card-title",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 20
+    },
+    __self: this
+  }, title), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("p", {
+    className: "card-text",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 21
+    },
+    __self: this
+  }, description), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("p", {
+    className: "card-text",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 22
+    },
+    __self: this
+  }, source.name), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("a", {
+    href: url,
+    target: "_blank",
+    className: "btn btn-primary d-block",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 23
+    },
+    __self: this
+  }, " Leer Noticia "))));
 };
 
 /* harmony default export */ __webpack_exports__["default"] = (Noticia);
